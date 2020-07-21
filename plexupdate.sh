780806ed5becf36217f82b73804c48532a6f8f36
Micheal Espinola <michealespinola@users.noreply.github.com>
2020-07-21 16:58:53 -0700

    Major overhault part 2 (tested with latest version)
    1. Explicitely install specific new filename so synopkg can install is (re: error 150)
    2. Allowing specific file name install now allows us to save our software versions within the same directory space
    3. Commented-out remove folder instruction as unneccessary
    4. Embellished echo'd messages to make more user readable for debugging
    5. Removed sleep as unneccesary. Called commands wait until finished before returning to script

--- plexupdate.sh
+++ plexupdate.sh
@@ -2,13 +2,14 @@
 #
 # Script to automagically update Plex Media Server on Synology NAS
 #
-# Must be run as admin.
+# Must be run as root to natively control running services.
 #
 # @author @martinorob https://github.com/martinorob
 # https://github.com/martinorob/plexupdate/
 #
 # @forked-author @michealespinola https://github.com/michealespinola
 # https://github.com/michealespinola/plexupdate
+# NEW VARIABLES IN CAPS BECAUSE I AM A CHILD
 #
 # Variables
 VOLUME="/volume1"
@@ -20,6 +21,7 @@ token=$(cat "$PLEX/Preferences.xml" | grep -oP 'PlexOnlineToken="\K[^"]+')
 url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
 jq=$(curl -s ${url})
 curversion=$(synopkg version "Plex Media Server")
+echo 
 echo "Current Version: $curversion"
 newversion=$(echo $jq | jq -r .nas.Synology.version)
 echo " Latest Version: $newversion"
@@ -27,21 +29,26 @@ dpkg --compare-versions "$newversion" gt "$curversion"
 if [ $? -eq "0" ]
 then
 echo 
-echo New version found..
+echo New version found...
+echo 
 /usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
 cpu=$(uname -m)
 if [ "$cpu" = "x86_64" ]; then
- url=$(echo $jq | jq -r ".nas.Synology.releases[1] | .url")
+  url=$(echo $jq | jq -r ".nas.Synology.releases[1] | .url"); FILE="${url##*/}"
 else
- url=$(echo $jq | jq -r ".nas.Synology.releases[0] | .url")
+  url=$(echo $jq | jq -r ".nas.Synology.releases[0] | .url"); FILE="${url##*/}"
 fi
-/bin/wget $url -P "$PLEX/Updates/plexupdate/"
-/usr/syno/bin/synopkg install "$PLEX/Updates/plexupdate/*.spk"
-sleep 30
+echo "    New Version: $newversion"
+echo "       New File: $FILE"
+echo 
+/bin/wget $url -c -nc -P "$PLEX/Updates/plexupdate/"
+/usr/syno/bin/synopkg stop "Plex Media Server"
+/usr/syno/bin/synopkg install "$PLEX/Updates/plexupdate/$FILE"
 /usr/syno/bin/synopkg start "Plex Media Server"
-rm -rf "$PLEX/Updates/plexupdate/*"
+# rm -rf "$PLEX/Updates/plexupdate/*"
 else
 echo 
-echo No new version found..
+echo No new version found.
+echo 
 fi
 exit
